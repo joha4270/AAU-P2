014028b2a9d860a0819e5940d9bc4ceb3ba6a575
Johannes Elgaard <joha4270@gmail.com>
2016-02-09 09:40:11 +0100

    SIMPLE code that makes music. If you have no MIDI output device you will probably crash

---
+++ code/ConsoleApplication/ConsoleApplication/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Midi;
+
+namespace ConsoleApplication
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int delay = 4;
+			OutputDevice outputDevice = OutputDevice.InstalledDevices[0];
+			if (outputDevice == null)
+			{
+				Console.WriteLine("No output devices, so can't run this example.");
+				return;
+			}
+			outputDevice.Open();
+
+			Console.WriteLine("Playing random MIDI");
+
+			Random r = new Random();
+
+			while (true)
+			{
+				int pitch = r.Next((int)Pitch.C3 - 1, (int)Pitch.B3);
+
+				if (pitch == (int)(Pitch.C3 - 1))
+				{
+					Thread.Sleep(1000 / delay);
+				}
+				else
+				{
+					outputDevice.SendNoteOn(Channel.Channel1, (Pitch)pitch, 80);
+
+					Thread.Sleep(1000 / delay);
+					outputDevice.SendNoteOff(Channel.Channel1, (Pitch)pitch, 80);
+				}
+			}
+
+		}
+	}
+}
