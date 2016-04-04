18d9a82a3ba9d69701d778c19733b3d6c2098b99
RegsaGC <asgerhb@gmail.com>
2016-04-04 10:51:42 +0200

    MIDI Gremlin born.

---
+++ code/MIDI Gremlin/MidiGremlinTests/monsterTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using MidiGremlin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidiGremlin.Tests
+{
+    [TestFixture()]
+    public class monsterTests
+    {
+        [Test()]
+        public void DoEverythingTest ()
+        {
+            Assert.Fail();
+        }
+    }
+}
